19b089b487767cc0de094ddaab9e829ec9d47e31
bloomfieldfong <bloomfieldf24@gmail.com>
2016-11-14 11:46:20 -0600

    Error

--- Errores.java
+++ Errores.java
@@ -1,6 +1,30 @@
 
+/**
+ * @author toshibas
+ *
+ */
 public class Errores {
 
+	/**
+	 * @param a
+	 */
 	
+	public boolean Error1001(String a){
+		boolean deci;
+		
+		try{
+	
+			int x = Integer.parseInt(a);
+			deci = true;
+			
+		}
+		catch(Exception e){
+			
+			deci = false; 
+			
+		}
+		return deci;
+	
+	}
 	
 }
